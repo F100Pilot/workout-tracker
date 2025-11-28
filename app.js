28d0d63b0fb19607b3f3d822db0a1e8238abb470
F100Pilot <72453076+F100Pilot@users.noreply.github.com>
2025-11-28 19:11:59 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// === CONFIG ===
 const SUPABASE_URL = 'https://pcpjsuzfbjbsztepcglw.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_P6awrwvBFtKqWq1Oeihgvg_FFgVBYF7';
 
 // === Inicialização Supabase ===
-const supabase = supabaseLib.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // DOM
 const btnLogin = document.getElementById('btn-login');
